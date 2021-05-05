4ea2c57697a6a010b24931edd1bb7da53ba927e1
Sergey Lavrinenko <s@lavr.me>
2021-05-05 12:59:39 +0300

    ...

--- .release/scripts/release-prepare.sh
+++ .release/scripts/release-prepare.sh
@@ -2,10 +2,20 @@
 set -x
 set -e
 
-CURRENT_PROJECT_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
-echo $CURRENT_PROJECT_VERSION
+while getopts v: flag
+do
+    case "${flag}" in
+        v) next_version=${OPTARG};;
+    esac
+done
 
-NEXT_PROJECT_VERSION=$(python .release/scripts/bump-version.py --current-version=$CURRENT_PROJECT_VERSION --suffixes=-SNAPSHOT)
+CURRENT_PROJECT_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+
+if [ -z $next_version ]; then
+  NEXT_PROJECT_VERSION=$(python .release/scripts/bump-version.py --current-version=$CURRENT_PROJECT_VERSION --suffixes=-SNAPSHOT)
+else
+  NEXT_PROJECT_VERSION=$next_version
+fi
 echo $NEXT_PROJECT_VERSION
 
 GIT_TAG_SUFFIX=$(date +%Y.%m.%d)
@@ -20,9 +30,9 @@ mkdir -p $RELEASE_DIR
 rm -f .release/releases/current
 ln -s ${NEXT_PROJECT_VERSION} .release/releases/current
 
-echo "PREV_TAG_REF=$PREV_TAG_REF" > $RELEASE_DIR/manifest.env
-echo "NEXT_GIT_TAG=$NEXT_GIT_TAG" >> $RELEASE_DIR/manifest.env
-echo "CURRENT_PROJECT_VERSION=$CURRENT_PROJECT_VERSION" >> $RELEASE_DIR/manifest.env
-echo "NEXT_PROJECT_VERSION=$NEXT_PROJECT_VERSION" >> $RELEASE_DIR/manifest.env
+echo "PREV_TAG_REF=$PREV_TAG_REF
+NEXT_GIT_TAG=$NEXT_GIT_TAG
+CURRENT_PROJECT_VERSION=$CURRENT_PROJECT_VERSION
+NEXT_PROJECT_VERSION=$NEXT_PROJECT_VERSION" > $RELEASE_DIR/manifest.env
 
 git log "$PREV_TAG_REF".. --reverse --format="* %s (%an)" > $RELEASE_DIR/changelog.md
