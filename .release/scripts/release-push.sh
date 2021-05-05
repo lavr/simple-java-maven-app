98ec744ee1d7330df7518933bc5c66f1ae68da04
Sergey Lavrinenko <s@lavr.me>
2021-05-05 13:07:01 +0300

    [TASK-1] пример коммита

--- .release/scripts/release-push.sh
+++ .release/scripts/release-push.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+set -x
+set -e
 
 source .release/releases/current/manifest.env
 
 git push
 git push --tags
-gh release create $NEXT_GIT_TAG -F .release/releases/current/changelog.md
+gh release create $NEXT_GIT_TAG -F .release/releases/current/changelog.md -R "$(git config --get remote.origin.url)"
